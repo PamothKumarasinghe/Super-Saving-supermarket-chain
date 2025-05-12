23cb11cbad688daafe4737eae713d1c8f895f0fd
PamothKumarasinghe <nikolailaazz2@gmail.com>
2025-05-12 20:16:58 +0530

    I hv no idea

--- SSS.java
+++ SSS.java
@@ -1,3 +1,7 @@
+
+import java.io.*;
+import java.util.*;
+
 class ItemCodeNotFound extends Exception {
     public ItemCodeNotFound() {}
     public ItemCodeNotFound(String message) {
@@ -8,18 +12,17 @@ class GroceryItem {
     private String itemCode;
     private String itemName;
     private double itemPrice;
+    private String manufacturer;
     private String pDate;
     private String eDate;
 
-    public GroceryItem(String itemCode, String itemName, double itemPrice, int itemQuantiy, String pDate, String eDate) {
+    public GroceryItem(String itemCode, String itemName, double itemPrice,String manufacturer, String pDate, String eDate) {
         this.itemCode = itemCode;
         this.itemName = itemName;
         this.itemPrice = itemPrice;
+        this.manufacturer = manufacturer;
         this.pDate = pDate;
         this.eDate = eDate;
-    }
-    public String getItemCode() {
-        return itemCode;
     }
     public double getItemPrice(int itemQuantity) {
         return itemPrice * itemQuantity;
@@ -27,11 +30,44 @@ class GroceryItem {
     
 }
 class POS {
+    static final HashMap<String, GroceryItem> cart = new HashMap<>();
+    Scanner sc = new Scanner(System.in);
+    
+    static {
+        try {
+            File file = new File("grocery_item.txt");
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line = br.readLine(); //read the first line in the file
+                
+            while (line != null) {
+                String[] arr = line.split(",");
+                String a = arr[0];
+                String b = arr[1];
+                double c = Double.parseDouble(arr[2]);
+                String d = arr[3];
+                String e = arr[4];
+                String f = arr[5];
 
-    startic 
-    public GroceryItem getItemDetails() {
+                cart.put(a, new GroceryItem(a, b, c, d, e, f));
+
+            }
+        } 
+        catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (IOException | NumberFormatException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void getItemDetails() {
+        System.out.println("Enter the item code:");
+        String itemCode = sc.nextLine();
+
+
 
     }
+    
 }
 public class SSS {
     public static void main(String[] args) {
