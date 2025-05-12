e76393fb68915dd5a4b81b7ecfd77ab722e539c9
PamothKumarasinghe <nikolailaazz2@gmail.com>
2025-05-12 16:10:44 +0530

    1_ItemCodeNotFound

---
+++ POS.java
@@ -0,0 +1,12 @@
+class ItemCodeNotFound extends Exception {
+    public ItemCodeNotFound() {}
+    public ItemCodeNotFound(String message) {
+        super(message);
+    }
+}
+
+public class POS {
+    public static void main(String[] args) {
+
+    }
+}
