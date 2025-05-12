b61beff16b7223144d78e2e1f92311c7d3d52dd7
PamothKumarasinghe <nikolailaazz2@gmail.com>
2025-05-12 17:06:31 +0530

    groceryItems

--- SSS.java
+++ SSS.java
@@ -4,7 +4,35 @@ class ItemCodeNotFound extends Exception {
         super(message);
     }
 }
+class GroceryItem {
+    private String itemCode;
+    private String itemName;
+    private double itemPrice;
+    private String pDate;
+    private String eDate;
 
+    public GroceryItem(String itemCode, String itemName, double itemPrice, int itemQuantiy, String pDate, String eDate) {
+        this.itemCode = itemCode;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.pDate = pDate;
+        this.eDate = eDate;
+    }
+    public String getItemCode() {
+        return itemCode;
+    }
+    public double getItemPrice(int itemQuantity) {
+        return itemPrice * itemQuantity;
+    }
+    
+}
+class POS {
+
+    startic 
+    public GroceryItem getItemDetails() {
+
+    }
+}
 public class SSS {
     public static void main(String[] args) {
 
